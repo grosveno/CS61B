0f4254de75215946b400090fb1e89895961cb682
Zihan Yan <647979893@qq.com>
2024-09-21 18:07:12 +0800

    modify the random

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,34 +2,55 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDequeGold {
-
     @Test
-    public void TestArrayDeque() {
-        StudentArrayDeque<Integer> lst1 = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> lst2 = new ArrayDequeSolution<>();
-        String message = "";
-        for (int i = 0; i < 50; i++) {
-            double rate = StdRandom.uniform();
-
-            if (rate < 0.25) {
-                lst1.addFirst(i);
-                lst2.addFirst(i);
-                message = String.format("%s addFirst(%d)\n", message, i);
-            } else if (rate < 0.5){
-                lst1.addLast(i);
-                lst2.addLast(i);
-                message = String.format("%s addLast(%d)\n", message, i);
-            } else if (rate < 0.75 && !lst1.isEmpty()&& !lst2.isEmpty()) {
-                Integer a = lst1.removeFirst();
-                Integer b = lst2.removeFirst();
-                message = String.format("%s removeFirst()\n", message);
-                assertEquals(message, a, b);
-            } else if (!lst1.isEmpty() && !lst2.isEmpty()) {
-                Integer a = lst1.removeLast();
-                Integer b = lst2.removeLast();
-                message = String.format("%s removeLast()\n", message);
-                assertEquals(message, a, b);
+    public void testStudentArrayDeque() {
+        StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> stdArray = new ArrayDequeSolution<>();
+        String log = "";
+        for (int i = 0; i < 1000; i++) {
+            if (stdArray.isEmpty()) {
+                int addNumber = StdRandom.uniform(1000);
+                int headOrBack = StdRandom.uniform(2);
+                if (headOrBack == 0) {
+                    log = String.format("%s addFirst(%d)\n", log, addNumber);
+                    testArray.addFirst(addNumber);
+                    stdArray.addFirst(addNumber);
+                } else {
+                    log = String.format("%s addLast(%d)\n", log, addNumber);
+                    testArray.addLast(addNumber);
+                    stdArray.addLast(addNumber);
+                }
+            } else {
+                int x = StdRandom.uniform(4);
+                int addNumber = StdRandom.uniform(1000);
+                Integer testremoveNumber = 1;
+                Integer stdremoveNumber = 1;
+                switch (x) {
+                    case 0:
+                        log = String.format("%s addFirst(%d)\n", log, addNumber);
+                        testArray.addFirst(addNumber);
+                        stdArray.addFirst(addNumber);
+                        break;
+                    case 1:
+                        log = String.format("%s addLast(%d)\n", log, addNumber);
+                        testArray.addLast(addNumber);
+                        stdArray.addLast(addNumber);
+                        break;
+                    case 2:
+                        log = String.format("%s removeFirst()\n", log);
+                        testremoveNumber = testArray.removeFirst();
+                        stdremoveNumber = stdArray.removeFirst();
+                        break;
+                    case 3:
+                        log = String.format("%s removeLast()\n", log);
+                        testremoveNumber = testArray.removeLast();
+                        stdremoveNumber = stdArray.removeLast();
+                        break;
+                    default:
+                }
+                assertEquals(log, stdremoveNumber, testremoveNumber);
             }
         }
+
     }
 }
